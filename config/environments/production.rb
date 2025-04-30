e13b20ee6a543829430603a4e14a8991b7d4e1cf
nekoharuki <harukiyamada1212@gmail.com>
2025-04-30 15:01:13 +0900

    awsにコミット

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,10 @@
 # config/environments/production.rb
 
 Rails.application.configure do
-    config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-    config.assets.compile = false
-    config.log_level = :info
-    config.require_master_key = true
-  end
-  
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.assets.compile = false
+  config.log_level = :info
+  config.eager_load = true
+
+  config.require_master_key = true
+end
